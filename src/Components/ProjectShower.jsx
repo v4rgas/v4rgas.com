ef2f261aa5e8d178fd457920c307dfcc9f9f041d
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-10-01 20:34:10 -0300

    refactor: Add preact-markdown package and update project structure and styling

--- src/Components/ProjectShower.jsx
+++ src/Components/ProjectShower.jsx
@@ -1,13 +1,27 @@
 import './ProjectShower.css';
 
+import { useEffect, useState } from 'preact/hooks';
+
+import Markdown from 'preact-markdown';
+
 export default function ProjectShower({ project }) {
+    // get project markdown
+    const [projectMarkdown, setProjectMarkdown] = useState('')
+
+    const getProjectMarkdown = async () => {
+        const response = await fetch(`https://raw.githubusercontent.com/v4rgas/sortEm/refs/heads/main/README.md`)
+        const data = await response.text()
+        setProjectMarkdown(data)
+    }
+
+    useEffect(() => {
+        getProjectMarkdown()
+    }, [])
+
     return (
         <div className='project-shower'>
-            <span className='project-name'>
-                {project.name}
-            </span>
-            <span className='project-description'>
-                {project.description}
+            <span className='project-md'>
+                <Markdown markdown={projectMarkdown} markedOpts={{ gfm: true }} />
             </span>
         </div>
     )
