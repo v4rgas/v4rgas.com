ffc6c85b18bf21eb1d6afb92e0540305a30828e8
v4rgas <66626747+v4rgas@users.noreply.github.com>
2024-10-02 19:08:01 -0300

    Refactor useGithubFetcher to cache fetched data
    This commit refactors the useGithubFetcher hook to implement caching of fetched data. It introduces a cache object using the Map data structure and modifies the fetchText function to check the cache before making a network request. If the requested URL is found in the cache, the cached data is returned instead of making a new request. This optimization improves performance by reducing unnecessary network calls.
    
    Additionally, the commit adds an useEffect hook to fetch the readme content for each project in the PROJECTS array defined in the assets/projects.json file. This ensures that the readme content is preloaded and available when needed.
    
    The changes in this commit enhance the efficiency and responsiveness of the useGithubFetcher hook.

--- src/useGithubFetcher.jsx
+++ src/useGithubFetcher.jsx
@@ -1,20 +1,31 @@
-import { useState } from "preact/hooks"
+import { useEffect, useState } from "preact/hooks"
+
+import PROJECTS from "./assets/projects.json"
+
+const cache = new Map()
 
 const useGithubFetcher = () => {
-    const [fetchCache, setFetchCache] = useState({})
+    // const [fetchCache, setFetchCache] = useState({})
 
     function fetchText(url) {
-        if (fetchCache[url]) {
+        if (cache.has(url)) {
             console.log('fetching from cache')
-            return Promise.resolve(fetchCache[url])
+            return Promise.resolve(cache.get(url))
         }
+
         return fetch(url)
             .then(response => response.text())
             .then(text => {
-                setFetchCache({ ...fetchCache, [url]: text })
+                cache.set(url, text)
                 return text
             })
     }
+
+    useEffect(() => {
+        PROJECTS.forEach(project => {
+            fetchText(project.readmeUrl)
+        })
+    }, [])
 
     function getProjectReadme(project) {
         return fetchText(project.readmeUrl)
