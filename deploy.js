b93f8012c9165045e31dde20a177e2104197a4b9
v4rgas <66626747+v4rgas@users.noreply.github.com>
2026-02-20 23:45:49 -0300

    Remove deploy script, use Cloudflare Pages build

--- deploy.js
+++
@@ -1,9 +0,0 @@
-import { execSync } from 'child_process';
-
-const run = (cmd) => execSync(cmd, { stdio: 'inherit' });
-
-run('git add -A');
-run('git commit -m "Update site"');
-run('git push origin main');
-
-console.log('Pushed! Cloudflare will build and deploy.');
